18db9610653abac8cf5f0d541759da898f469d0c
unknown <mouni.shalaby@gmail.com>
2023-02-24 20:10:47 +0200

    Output Users forEach & DOM Methods

--- DOM_array_methods/script.js
+++ DOM_array_methods/script.js
@@ -24,6 +24,30 @@ async function getRandomUser() {
 }
 
 //Add new obj to data arr
-function addData(){
-    data.push(obj)
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
 }
+
+//update Dom
+function updateDOM(providedData = data) {
+  //clear main div
+  main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`;
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element)
+  });
+}
+
+//Format numer as money
+function formatMoney(number){
+  return '$'+ number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,"); // 12,345.67
+}
+
+//Event listner
+addUserBtn.addEventListener('click', ()=>{
+    getRandomUser()
+})
